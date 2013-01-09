368be15c1b891d1506d415b2fcb2d8e22b3f9302
Mark Cornelisse <markcornelisse@ZeeuwAir.local>
2013-01-09 07:19:39 +0100

    Initial Commit

---
+++ Going Dutch/MCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCAppDelegate.h
+//  Going Dutch
+//
+//  Created by Mark Cornelisse on 09-01-13.
+//  Copyright (c) 2013 Mark Cornelisse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Going Dutch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Going Dutch
+//
+//  Created by Mark Cornelisse on 09-01-13.
+//  Copyright (c) 2013 Mark Cornelisse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCAppDelegate class]));
+    }
+}
