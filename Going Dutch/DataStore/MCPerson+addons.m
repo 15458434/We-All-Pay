a8f24a00d5299527711b15bcfeb641f4eb170968
Mark Cornelisse <info@markcornelisse.nl>
2013-12-14 14:00:38 +0100

    Resolved #69 - 64-bit support has been added for iPhone 5s.

--- Going Dutch/DataStore/MCPerson+addons.m
+++ Going Dutch/DataStore/MCPerson+addons.m
@@ -159,7 +159,7 @@
     if (!emailAddresses) {
         NSLog(@"Something went wrong on fetching emailAddresses: %@", [error localizedDescription]);
     } else if ([emailAddresses count] != 1) {
-        NSLog(@"%d defaultEmailAddresses found.", [emailAddresses count]);
+        NSLog(@"%lu defaultEmailAddresses found.", [emailAddresses count]);
     }
     return [emailAddresses firstObject];
 }
--- Going Dutch/ViewControllers/MCEditTripViewController.m
+++ Going Dutch/ViewControllers/MCEditTripViewController.m
@@ -171,9 +171,9 @@
 - (void)updateSubLabel
 {
     if ([[dataController fetchedObjects] count] == 1) {
-        [[twoLabelTitleView subLabel] setText:[NSString stringWithFormat:@"%d person present", [[dataController fetchedObjects] count]]];
+        [[twoLabelTitleView subLabel] setText:[NSString stringWithFormat:@"%lu person present", [[dataController fetchedObjects] count]]];
     } else {
-        [[twoLabelTitleView subLabel] setText:[NSString stringWithFormat:@"%d people present", [[dataController fetchedObjects] count]]];
+        [[twoLabelTitleView subLabel] setText:[NSString stringWithFormat:@"%lu people present", [[dataController fetchedObjects] count]]];
     }
     if (SYSTEM_VERSION_LESS_THAN(@"7.0")) {
         [[twoLabelTitleView mainLabel] setTextColor:[UIColor whiteColor]];
