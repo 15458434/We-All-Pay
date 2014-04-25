3a57eef40fc4ad808fee938bf597e585a22568b0
Mark Cornelisse <info@markcornelisse.nl>
2014-04-25 14:07:14 +0200

    #182 payment and person tableViewCell has been updated for the iPhone.

--- Going Dutch/ViewControllers/MCSharedBillTableViewController.m
+++ Going Dutch/ViewControllers/MCSharedBillTableViewController.m
@@ -383,7 +383,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 50;
+    return 60;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
