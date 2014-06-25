0c560144ff312336670e037a831d6964f750008c
Mark Cornelisse <info@markcornelisse.nl>
2014-06-25 15:59:40 +0200

    Resolved #250

---
+++ Going Dutch/ViewControllers/iPad/MCiScreenRootViewController_iPad.h
@@ -0,0 +1,13 @@
+//
+//  MCiScreenRootViewController_iPad.h
+//  We all pay
+//
+//  Created by Mark Cornelisse on 25-06-14.
+//  Copyright (c) 2014 Mark Cornelisse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCiScreenRootViewController_iPad : UIViewController
+
+@end
--- Going Dutch/ViewControllers/iPad/MCiScreenTableViewController_iPad.m
+++ Going Dutch/ViewControllers/iPad/MCiScreenTableViewController_iPad.m
@@ -85,7 +85,7 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    [[self tableView] setBackgroundColor:[MCColors getbackgroundColor]];
+    //[[self tableView] setBackgroundColor:[MCColors getbackgroundColor]];
     
     numberOfRowsInSection0 = [self getAmountOfRowsInSection0];
 }
