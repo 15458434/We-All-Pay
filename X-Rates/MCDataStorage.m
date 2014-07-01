5c63257d08406b3710aa8ec53a6a0b8ee5d181d3
Mark Cornelisse <info@markcornelisse.nl>
2014-07-01 17:48:37 +0200

    Resolved #271

--- X-Rates/MCDataStorage.m
+++ X-Rates/MCDataStorage.m
@@ -33,12 +33,12 @@
     NSString *sourceCurrencyISOCode = [[[_sourceController selectedObjects] firstObject] valueForKeyPath:@"currencyISOCode"];
     NSString *destinationCurrencyISOCode = [[[_destinationController selectedObjects] firstObject] valueForKey:@"currencyISOCode"];
     [_xRatesController getExchangeRateFrom:sourceCurrencyISOCode to:destinationCurrencyISOCode withCompletionHandler:^(NSDictionary *exchangeRateResult) {
-//        NSNumberFormatter *numberFormatter = [NSNumberFormatter new];
-//        NSString *localeIdentifier = [exchangeRateResult valueForKeyPath:@"query.lang"];
-//        [numberFormatter setLocale:[NSLocale localeWithLocaleIdentifier:localeIdentifier]];
-//        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
-//        [self setExchangeRate:[numberFormatter numberFromString:[exchangeRateResult valueForKeyPath:@"query.results.row.rate"]]];
-        self.exchangeRate = [exchangeRateResult valueForKeyPath:@"query.results.row.rate"];
+        NSNumberFormatter *numberFormatter = [NSNumberFormatter new];
+        NSString *localeIdentifier = [exchangeRateResult valueForKeyPath:@"query.lang"];
+        [numberFormatter setLocale:[NSLocale localeWithLocaleIdentifier:localeIdentifier]];
+        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+        [self setExchangeRate:[numberFormatter numberFromString:[exchangeRateResult valueForKeyPath:@"query.results.row.rate"]]];
+//        self.exchangeRate = [exchangeRateResult valueForKeyPath:@"query.results.row.rate"];
         if (_exchangeRate) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self setDestinationAmount:@([_sourceAmount doubleValue] * [_exchangeRate doubleValue])];
