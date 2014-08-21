48ec897a2f3a8c14c2063b2a59364039ab84e2ae
Mark Cornelisse <info@markcornelisse.nl>
2014-08-21 16:56:45 +0200

    Resolved #355

--- Going Dutch/DataStore/MCWeAllPayStoreController.m
+++ Going Dutch/DataStore/MCWeAllPayStoreController.m
@@ -26,7 +26,7 @@ NSString * const MCWeAllPayStoreDirectoryName = @"WeAllPayStore/StoreContent";
 // File of the WeAllPayStore Database model file.
 NSString * const MCWeAllPayStoreModelName = @"WeAllPayStore";
 
-NSString * const MCiCloudWeAllPayStoreName = @"iCloud-WeAllPayStore6";
+NSString * const MCiCloudWeAllPayStoreName = @"iCloud-WeAllPayStore";
 
 @interface MCWeAllPayStoreController ()
 
