de93c9133e7a277475932b17c5010892361a19dd
Mark Cornelisse <info@markcornelisse.nl>
2014-11-18 12:39:11 +0100

    Resolved #484

---
+++ Going Dutch/DataStore/Entity Migration Policies/MCPaymentUpdateTo4.m
@@ -0,0 +1,24 @@
+//
+//  MCPaymentUpdateTo4.m
+//  We all pay
+//
+//  Created by Mark Cornelisse on 18/11/14.
+//  Copyright (c) 2014 Mark Cornelisse. All rights reserved.
+//
+
+#import "MCPaymentUpdateTo4.h"
+#import "MCPayment.h"
+
+@implementation MCPaymentUpdateTo4
+
+- (BOOL)createRelationshipsForDestinationInstance:(NSManagedObject *)dInstance entityMapping:(NSEntityMapping *)mapping manager:(NSMigrationManager *)manager error:(NSError *__autoreleasing *)error
+{
+    MCPayment *paymentInDestinationInstance = (MCPayment *)dInstance;
+    paymentInDestinationInstance.categoryId = @(10);
+#if DEBUG
+    NSLog(@"%@", paymentInDestinationInstance);
+#endif
+    return YES;
+}
+
+@end
