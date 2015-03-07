9ffdc19c4eb0be8a4c1f22263669dfcd31ac1142
Mark Cornelisse <info@markcornelisse.nl>
2015-03-07 12:42:12 +0100

    - Solved a compile warning and provision profile issues

--- Going Dutch/ViewControllers/MCPaymentViewController.m
+++ Going Dutch/ViewControllers/MCPaymentViewController.m
@@ -313,7 +313,7 @@ typedef NS_ENUM(BOOL, ChildViewOpened) {
 {
     if (textField == paidView) {
 #if DEBUG
-        NSLog(@"kindOfPaidFieldDismiss = %lu", kindOfPaidFieldDismiss);
+        NSLog(@"kindOfPaidFieldDismiss = %u", kindOfPaidFieldDismiss);
 #endif
         if (kindOfPaidFieldDismiss == cancelIsPressed) {
             // Restore stored value
