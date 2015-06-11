450927e3e13114d2ffdfe3f7111aa8b56142345c
Mark Cornelisse <info@markcornelisse.nl>
2015-06-11 23:17:28 +0200

    - Converted MCSolutionOverViewTableViewCell_iPhone to a Swift class

--- Going Dutch/ViewControllers/MCReturnPaymentViewController.m
+++ Going Dutch/ViewControllers/MCReturnPaymentViewController.m
@@ -16,7 +16,6 @@
 #import "MCPerson+addons.h"
 #import "MCWeAllPayStoreController.h"
 
-#import "MCSolutionOverViewTableViewCell_iPhone.h"
 #import "MCTwoLabelsTitleView.h"
 
 #import "We_all_pay-Swift.h"
