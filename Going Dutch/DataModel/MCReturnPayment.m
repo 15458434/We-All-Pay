ea11983afcc9ea608c8e221b15f1943049a36c66
Mark Cornelisse <info@markcornelisse.nl>
2015-06-12 11:39:13 +0200

    - More cleaning.

--- Going Dutch/DataModel/MCReturnPayment.m
+++ Going Dutch/DataModel/MCReturnPayment.m
@@ -30,7 +30,6 @@
     NSNumberFormatter *nf = [currency numberFormatter];
     [nf setNumberStyle:NSNumberFormatterCurrencyStyle];
     
-    
     NSString *owesString1 = NSLocalizedString(@"EMAIL_OWES_PART_ONE", @"Part one of the words: %@ pays %@ to %@.");
     NSString *owesString2 = NSLocalizedString(@"EMAIL_OWES_PART_TWO", @"Part two of the words: %@ pays %@ to %@.");
     return [[NSString alloc] initWithFormat:@"%@ %@ %@ %@ %@.", [_payer firstName], owesString1, [nf stringFromNumber:_money], owesString2, [_receiver firstName]];
