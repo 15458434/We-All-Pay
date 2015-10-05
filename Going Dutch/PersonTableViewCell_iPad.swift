28ebc024524082cce39193652f292e351c8d9fd8
Mark Cornelisse <info@markcornelisse.nl>
2015-10-05 14:51:00 +0200

    Converted PersonTableViewCell_iPad to Swift

---
+++ Going Dutch/PersonTableViewCell_iPad.swift
@@ -0,0 +1,17 @@
+//
+//  PersonTableViewCell_iPad.swift
+//  We all pay
+//
+//  Created by Mark Cornelisse on 05/10/15.
+//  Copyright © 2015 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell_iPad: UITableViewCell {
+    // MARK: IB Outlets
+    @IBOutlet var personImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var totalSpent: UILabel!
+}
--- Going Dutch/ViewControllers/iPad/MCSharedBillPeoplePresentTableViewController-iPad.m
+++ Going Dutch/ViewControllers/iPad/MCSharedBillPeoplePresentTableViewController-iPad.m
@@ -9,8 +9,6 @@
 #import "MCSharedBillPeoplePresentTableViewController-iPad.h"
 #import "UIViewController+WeAllPayStore.h"
 
-#import "MCPersonTableViewCell_iPad.h"
-
 #import "MCPerson+addons.h"
 #import "MCSharedBill+addons.h"
 
@@ -264,7 +262,7 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     MCPerson *thisCellsPerson = [_dataController objectAtIndexPath:indexPath];
-    MCPersonTableViewCell_iPad *thisCell = [tableView dequeueReusableCellWithIdentifier:@"MCPersonTableViewCell_iPad"];
+    PersonTableViewCell_iPad *thisCell = [tableView dequeueReusableCellWithIdentifier:@"MCPersonTableViewCell_iPad"];
     
     thisCell.personImage.image = thisCellsPerson.picture;
     [[thisCell nameLabel] setText:[thisCellsPerson getFullName]];
