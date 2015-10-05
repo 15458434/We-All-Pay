deee8bfb539c314262699b05b2b49e48c8a06601
Mark Cornelisse <info@markcornelisse.nl>
2015-10-05 20:00:56 +0200

    Converted SelectCategoryTableViewCell_iPad to Swift

---
+++ Going Dutch/SelectCategoryTableViewCell_iPad.swift
@@ -0,0 +1,15 @@
+//
+//  SelectCategoryTableViewCell_iPad.swift
+//  We all pay
+//
+//  Created by Mark Cornelisse on 05/10/15.
+//  Copyright © 2015 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+
+class SelectCategoryTableViewCell_iPad: UITableViewCell {
+    // MARK: IB Outlets
+    @IBOutlet var categoryImageView: UIImageView!
+    @IBOutlet var categoryNameLabel: UILabel!
+}
