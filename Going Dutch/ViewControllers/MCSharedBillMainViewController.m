cdabdc89e00efde4699f651cec34890aff477fa9
Mark Cornelisse <info@markcornelisse.nl>
2016-07-01 17:52:58 +0200

    - Added iPad advertising identifier.

--- Going Dutch/ViewControllers/MCSharedBillMainViewController.m
+++ Going Dutch/ViewControllers/MCSharedBillMainViewController.m
@@ -42,7 +42,8 @@
 #if DEBUG
     NSString *kiPhone5S = @"109c8d87d59d27b62a53157e313d1a49";
     NSString *kiPhone4S = @"87ebfc252a3675f03375aa13fce9286f";
-    request.testDevices = @[kGADSimulatorID, kiPhone5S, kiPhone4S];
+    NSString *iPadRetina = @"63f51db641e29b85012042e407de3cba";
+    request.testDevices = @[kGADSimulatorID, kiPhone5S, kiPhone4S, iPadRetina];
 #endif
     return request;
 }
