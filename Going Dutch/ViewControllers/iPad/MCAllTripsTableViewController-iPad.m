147555600481ce5b4103ef71bb2859ed486d8a00
Mark Cornelisse <info@markcornelisse.nl>
2016-07-12 15:13:56 +0200

    Resolved #675

--- Going Dutch/ViewControllers/iPad/MCAllTripsTableViewController-iPad.m
+++ Going Dutch/ViewControllers/iPad/MCAllTripsTableViewController-iPad.m
@@ -74,15 +74,18 @@
 
 - (void)prepareUserActivity
 {
-    NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType:@"com.GreenHair.We-all-pay.SharingExpenses"];
-    activity.title = NSLocalizedString(@"We all pay - Sharing Expenses and bill splitting made easy", @"The title of the app");
-    NSString *keywordsFilePath = [[NSBundle mainBundle] pathForResource:@"We all pay keywords" ofType:@"plist"];
-    activity.keywords = [NSSet setWithArray:[NSArray arrayWithContentsOfFile:keywordsFilePath]];
-    activity.eligibleForHandoff = NO;
-    activity.eligibleForSearch = YES;
-    activity.eligibleForPublicIndexing = YES;
-    activity.requiredUserInfoKeys = [[NSSet alloc] init];
-    self.userActivity = activity;
+    NSOperatingSystemVersion ios9 = (NSOperatingSystemVersion){9, 0, 0};
+    if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:ios9]) {
+        NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType:@"com.GreenHair.We-all-pay.SharingExpenses"];
+        activity.title = NSLocalizedString(@"We all pay - Sharing Expenses and bill splitting made easy", @"The title of the app");
+        NSString *keywordsFilePath = [[NSBundle mainBundle] pathForResource:@"We all pay keywords" ofType:@"plist"];
+        activity.keywords = [NSSet setWithArray:[NSArray arrayWithContentsOfFile:keywordsFilePath]];
+        activity.eligibleForHandoff = NO;
+        activity.eligibleForSearch = YES;
+        activity.eligibleForPublicIndexing = YES;
+        activity.requiredUserInfoKeys = [[NSSet alloc] init];
+        self.userActivity = activity;
+    }
 }
 
 #pragma mark - Inherited from super
@@ -130,7 +133,9 @@
         }
     }
     
-    [[self userActivity] becomeCurrent];
+    if (self.userActivity) {
+        [self.userActivity becomeCurrent];
+    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated
