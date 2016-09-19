945a367f4f7fcc3b2bfeaa0ff554e6b4a148efec
Mark Cornelisse <info@markcornelisse.nl>
2016-09-19 20:11:45 +0200

    Resolved #722

--- Going Dutch/ViewControllers/MCPersonViewController.m
+++ Going Dutch/ViewControllers/MCPersonViewController.m
@@ -265,23 +265,6 @@ typedef NS_ENUM(BOOL, MCStatus) {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-//
-//#pragma mark - NSNotifications
-//
-//- (void)writableTonightsBillIsCreated:(NSNotification *)notification
-//{
-//    // Should be executed on the background thread.
-//    _writableTonightsBill = [[notification userInfo] objectForKey:MCwritableTonightsBillKey];
-//    _writableThisPerson = [_writableTonightsBill addPerson];
-//    NSLog(@"MCPersonViewController: writableTonightsBill is created.");
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        if (strongSelf) {
-//            [strongSelf fillTheScreenWithInitialData];
-//        }
-//    });
-//}
 
 #pragma mark - UITextFieldDelegate
 
