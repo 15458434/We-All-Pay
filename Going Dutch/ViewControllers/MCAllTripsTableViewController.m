ea2ca97ce5e3b5fb20b32cd9c147db1f72e6518d
Mark Cornelisse <info@markcornelisse.nl>
2018-07-10 18:54:59 +0200

    - Fixed a crash where leaving the event name blank results in a crash.

--- Going Dutch/ViewControllers/MCAllTripsTableViewController.m
+++ Going Dutch/ViewControllers/MCAllTripsTableViewController.m
@@ -341,7 +341,10 @@ typedef NS_ENUM(BOOL, MCTonightsBillStatus) {
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     MCSharedBill *selectedEvent = [_dataController objectAtIndexPath:indexPath];
-    [FIRAnalytics logEventWithName:@"Open event" parameters:@{@"Event name": selectedEvent.tripName}];
+    if (selectedEvent.tripName) {
+        [FIRAnalytics logEventWithName:@"Open event" parameters:@{@"Event name": selectedEvent.tripName}];
+    }
+    
 }
 
 /*
