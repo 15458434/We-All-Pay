ae6240e0d4546619c475c884800ebb439d0e9f4a
Mark Cornelisse <info@markcornelisse.nl>
2019-10-18 23:23:04 +0200

    - Removed Pods/

--- Pods/Target Support Files/Pods-We all pay Tests/Pods-We all pay Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_We_all_pay_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_We_all_pay_Tests
-@end
--- Pods/Target Support Files/Pods-We all pay/Pods-We all pay-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_We_all_pay : NSObject
-@end
-@implementation PodsDummy_Pods_We_all_pay
-@end
