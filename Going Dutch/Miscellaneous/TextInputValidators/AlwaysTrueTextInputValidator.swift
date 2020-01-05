f5271e3efa52207bb23352ee4785e51ad52bb860
Mark Cornelisse <m.p.cornelisse@gmail.com>
2020-01-05 18:46:17 +0100

    - Added an AlwaysTrueTextInputValidator

---
+++ Going Dutch/Miscellaneous/TextInputValidators/AlwaysTrueTextInputValidator.swift
@@ -0,0 +1,13 @@
+//
+//  AlwaysTrueTextInputValidator.swift
+//  We all pay
+//
+//  Created by Mark Cornelisse on 05/01/2020.
+//  Copyright © 2020 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+
+class AlwaysTrueTextInputValidator: TextInputValidator {
+
+}
