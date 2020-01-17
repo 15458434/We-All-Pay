f3dd6f66220552d20a5c02107d0ef815c60b4023
Mark Cornelisse <m.p.cornelisse@gmail.com>
2020-01-17 12:42:28 +0100

    - Small improvements to make the code compile faster.

--- Going Dutch/Miscellaneous/ContactsDataReceiver.swift
+++ Going Dutch/Miscellaneous/ContactsDataReceiver.swift
@@ -39,13 +39,13 @@ class ContactsDataReceiver: NSObject, ThisEventReadOnly, CNContactPickerDelegate
     
     // MARK: CNContactPickerDelegate
     
-    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
-        let tonightsBillID = event.objectID
-        let backgroundContext = MCWeAllPayStoreController.defaultStore().backgroundThreadContext!
+    @objc func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        let tonightsBillID: NSManagedObjectID = event.objectID
+        let backgroundContext: NSManagedObjectContext = MCWeAllPayStoreController.defaultStore().backgroundThreadContext!
         
         backgroundContext.perform {
-            let backgroundTonightsBill = backgroundContext.object(with: tonightsBillID) as! MCSharedBill
-            let newPerson = backgroundTonightsBill.addPerson()!
+            let backgroundTonightsBill: MCSharedBill = backgroundContext.object(with: tonightsBillID) as! MCSharedBill
+            let newPerson: MCPerson = backgroundTonightsBill.addPerson()!
             newPerson.firstName = contact.givenName
             newPerson.lastName = contact.middleName + contact.familyName
             for emailAddress in contact.emailAddresses {
