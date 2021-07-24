daa25492ee143de2001534bed2389de2126cc202
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-07-24 10:54:16 +0200

    - some cleaning.

--- Going Dutch/ViewControllers/iPad/MCAllTripsTableViewController-iPad.m
+++ Going Dutch/ViewControllers/iPad/MCAllTripsTableViewController-iPad.m
@@ -11,6 +11,8 @@
 #import "MCAllTripsTableViewController-iPad.h"
 #import "UIViewController+WeAllPayStore.h"
 
+#import "MCRoundedButton.h"
+
 #import "MCWeAllPayStoreController.h"
 #import "MCSharedBill+addons.h"
 #import "MCCurrency+addons.h"
@@ -52,13 +54,13 @@
 }
 
 - (void)setEmptyMessage {
-    if ([[_dataController fetchedObjects] count] != 0) {
+    if (_dataController.fetchedObjects.count != 0) {
         [UIView animateWithDuration:1.0 animations:^{
             self.emptyMessage.bigMessage.alpha = 0.0;
             self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
         } completion:nil];
     } else {
-        if ([[_emptyMessage bigMessage] alpha] < 1.0) {
+        if (_emptyMessage.bigMessage.alpha < 1.0) {
             [UIView animateWithDuration:1.0 animations:^{
                 self.emptyMessage.bigMessage.alpha = 1.0;
                 self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
@@ -74,7 +76,7 @@
             self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
         } completion:nil];
     } else {
-        if ([[_emptyMessage bigMessage] alpha] < 1.0) {
+        if (_emptyMessage.bigMessage.alpha < 1.0) {
             [UIView animateWithDuration:0.0 animations:^{
                 self.emptyMessage.bigMessage.alpha = 1.0;
                 self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
@@ -138,7 +140,7 @@
     }
 }
 
-#pragma mark UITableViewController
+#pragma mark - UITableViewController
 
 #pragma mark - UITableViewDelegate
 
@@ -213,15 +215,13 @@
 }
 
 // Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
 
 // Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
         [self deleteBillAtIndexpath:indexPath];
@@ -232,8 +232,7 @@
 
 #pragma mark - UIViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     
     // Uncomment the following line to preserve selection between presentations.
@@ -253,8 +252,7 @@
     [self prepareUserActivity];
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
     if (!_dataController) {
@@ -274,15 +272,13 @@
     }
 }
 
-- (void)viewWillDisappear:(BOOL)animated
-{
+- (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     
     _dataController = nil;
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
     
