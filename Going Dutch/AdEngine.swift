f4d944f6dd62be0086b18e31a7e02e2064c38173
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-08-14 11:00:26 +0200

    #922 - Added new consent form and deleted the old stuff.

--- Going Dutch/AdEngine.swift
+++ Going Dutch/AdEngine.swift
@@ -19,6 +19,7 @@ import AppLovinSDK
 import MoPub
 
 @objc(MCAdEngine) @objcMembers open class AdEngine: NSObject {
+    // TODO: Remove on 14-08-2022
     static let kAdBannerConsent = "7DE9F9CF-B4B9-4DCB-94FC-F0FD62F432DC"
     
     #if SCREENSHOTS
@@ -68,112 +69,7 @@ import MoPub
         GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [(kGADSimulatorID as! String), iPhoneX, iPadRetina]
     }
     
-    private class func gdprConsentInMobi(economicArea: AdEngine.EconomicArea = .unknown, consentStatus: ConsentStatus) {
-        var economicAreaString: String {
-            switch economicArea {
-            case .unknown:
-                return "0"
-            case .eea:
-                return "1"
-            }
-        }
-        var consentString: String {
-            switch consentStatus {
-            case .personalized:
-                return "true"
-            default:
-                return "false"
-            }
-        }
-        
-        if let _ = GADMInMobiConsent.consent {
-            // Nothing todo.
-            return
-        }
-        
-        let consentDictionary: [String: String] = ["gdpr": economicAreaString, IM_GDPR_CONSENT_AVAILABLE: consentString]
-        GADMInMobiConsent.updateGDPRConsent(consentDictionary)
-    }
-    
-    private class func gdprConsentAdcolony(economicArea: AdEngine.EconomicArea = .unknown, consentStatus: ConsentStatus) {
-        var gdprRequired: Bool {
-            switch economicArea {
-            case .unknown:
-                return false
-            case .eea:
-                return true
-            }
-        }
-        var consentString: String {
-            switch consentStatus {
-            case .personalized:
-                return "1"
-            default:
-                return "0"
-            }
-        }
-        
-        let options = GADMediationAdapterAdColony.appOptions!
-        options.gdprRequired = gdprRequired
-        options.gdprConsentString = consentString
-    }
-    
-    private class func gdprConsentAppLovin(economicArea: AdEngine.EconomicArea = .unknown, consentStatus: ConsentStatus) {
-        var gdprRequired: Bool {
-            switch economicArea {
-            case .unknown:
-                return false
-            case .eea:
-                return true
-            }
-        }
-        var consentValue: Bool {
-            switch consentStatus {
-            case .personalized:
-                return true
-            default:
-                return false
-            }
-        }
-        
-        if gdprRequired {
-            ALPrivacySettings.setHasUserConsent(consentValue)
-        }
-    }
-    
-    private class func gdprConsentMoPub(economicArea: AdEngine.EconomicArea = .unknown, consentStatus: ConsentStatus) {
-        var gdprRequired: Bool {
-            switch economicArea {
-            case .unknown:
-                return false
-            case .eea:
-                return true
-            }
-        }
-        var consentValue: Bool {
-            switch consentStatus {
-            case .personalized:
-                return true
-            default:
-                return false
-            }
-        }
-        
-        let moPubConfig = MPMoPubConfiguration(adUnitIdForAppInitialization: "b63c7628b51e41c497d6159df9a922b0")
-        let moPubInstance = MoPub.sharedInstance()
-        moPubInstance.initializeSdk(with: moPubConfig, completion: nil)
-        
-        if gdprRequired {
-            if consentValue {
-                moPubInstance.grantConsent()
-            } else {
-                moPubInstance.revokeConsent()
-            }
-        }
-    }
-    
     @objc(presentPrivacyConsentRequestIfNecessaryFromViewController:) class func presentPrivacyConsentRequestIfNecessary(from viewController: UIViewController) {
-        
         guard AdEngine.isEnabled else {
             return
         }
@@ -182,60 +78,64 @@ import MoPub
             return
         }
         
-        PACConsentInformation.sharedInstance.debugGeography = .EEA
-        PACConsentInformation.sharedInstance.debugIdentifiers = ["00000000-0000-0000-0000-000000000000", "E0C4F2B0-1AD9-4FEE-B467-7DE63D5E8939"]
-        PACConsentInformation.sharedInstance.requestConsentInfoUpdate(forPublisherIdentifiers: ["pub-5354415674074435"]) { (error) in
+        // clean up old stuff in UserDefaults
+        UserDefaults.standard.removeObject(forKey: kAdBannerConsent)
+        PACConsentInformation.sharedInstance.reset()
+        
+        // Create a UMPRequestParameters object.
+        let parameters = UMPRequestParameters()
+        // Set tag for under age of consent. Here false means users are not under age.
+        parameters.tagForUnderAgeOfConsent = false
+        
+        let debugSettings = UMPDebugSettings()
+        debugSettings.testDeviceIdentifiers = ["00000000-0000-0000-0000-000000000000", "E0C4F2B0-1AD9-4FEE-B467-7DE63D5E8939"]
+        debugSettings.geography = .notEEA
+        parameters.debugSettings = debugSettings
+        
+        // Request an update to the consent information.
+        UMPConsentInformation.sharedInstance.requestConsentInfoUpdate(with: parameters, completionHandler: { error in
             guard error == nil else {
-                debugPrint("Consent info update failed.")
+                // Handle the error.
+                debugPrint("UMPConsentInformation.sharedInstance.requestConsentInfoUpdate error: \(error!)")
                 return
             }
             
-            if PACConsentInformation.sharedInstance.isRequestLocationInEEAOrUnknown {
-                debugPrint("InEEAorUnknown")
-                
-                let userDefaults = UserDefaults.standard
-                let storedConsentStatus = PACConsentStatus(rawValue: userDefaults.integer(forKey: AdEngine.kAdBannerConsent))!
-                let consentStatus = ConsentStatus(consentStatus: storedConsentStatus)
-                switch consentStatus {
-                case .personalized:
-                    gdprConsentInMobi(economicArea: .eea, consentStatus: consentStatus)
-                    gdprConsentAdcolony(economicArea: .eea, consentStatus: consentStatus)
-                    gdprConsentAppLovin(economicArea: .eea, consentStatus: consentStatus)
-                    gdprConsentMoPub(economicArea: .eea, consentStatus: consentStatus)
-                case .nonPersonalized:
-                    gdprConsentInMobi(economicArea: .eea, consentStatus: consentStatus)
-                    gdprConsentAdcolony(economicArea: .eea, consentStatus: consentStatus)
-                    gdprConsentAppLovin(economicArea: .eea, consentStatus: consentStatus)
-                    gdprConsentMoPub(economicArea: .eea, consentStatus: consentStatus)
-                default:
-                    guard let privacyUrl = URL(string: "https://www.iubenda.com/privacy-policy/7876418"),
-                          let form = PACConsentForm(applicationPrivacyPolicyURL: privacyUrl) else {
-                        print("incorrect privacy URL.")
-                        return
-                    }
-                    form.shouldOfferPersonalizedAds = true
-                    form.shouldOfferNonPersonalizedAds = true
-                    form.shouldOfferAdFree = false
-                    
-                    form.load { [unowned form, unowned viewController] (error) in
-                        guard error == nil else {
-                            print("error: \(error!)")
-                            fatalError("Unable to load consent form.")
+            // The consent information state was updated.
+            // You are now ready to check if a form is available.
+            switch UMPConsentInformation.sharedInstance.consentStatus {
+            case .required:
+                let formStatus = UMPConsentInformation.sharedInstance.formStatus
+                if formStatus == UMPFormStatus.available {
+                    UMPConsentForm.load(completionHandler: { form, loadError in
+                        guard loadError == nil else {
+                            // Handle the error
+                            debugPrint("UMPConsentForm.loadError: \(loadError!)")
+                            return
                         }
                         
-                        form.present(from: viewController) { (error, success) in
-                            let currentConsentStatus = PACConsentInformation.sharedInstance.consentStatus
-                            UserDefaults.standard.set(consentStatus.rawValue, forKey: AdEngine.kAdBannerConsent)
-                            let consentStatus = ConsentStatus(consentStatus: currentConsentStatus)
-                            gdprConsentInMobi(economicArea: .eea, consentStatus: consentStatus)
-                            gdprConsentAdcolony(economicArea: .eea, consentStatus: consentStatus)
-                            gdprConsentAppLovin(economicArea: .eea, consentStatus: consentStatus)
-                            gdprConsentMoPub(economicArea: .eea, consentStatus: consentStatus)
+                        // Present the form. You can also hold on to the reference to present
+                        // later.
+                        if UMPConsentInformation.sharedInstance.consentStatus == UMPConsentStatus.required {
+                            form?.present(from: viewController, completionHandler: { dismissError in
+                                guard dismissError == nil else {
+                                    debugPrint("form dismissed error: \(dismissError!)")
+                                    return
+                                }
+                                
+                                if UMPConsentInformation.sharedInstance.consentStatus == UMPConsentStatus.obtained {
+                                    // App can start requesting ads.
+                                    GADMobileAds.sharedInstance().start(completionHandler: nil)
+                                }
+                            })
+                        } else {
+                            // Keep the form available for changes to user consent.
                         }
-                    }
+                    })
                 }
+            default:
+                GADMobileAds.sharedInstance().start(completionHandler: nil)
             }
-        }
+        })
     }
     
     var request: GADRequest {
