e3eb7450011d2068238f18d9cd9fc0e072d5fd51
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-08-17 19:19:55 +0200

    #918 - Disconnected the last items in de iPad.storyboard.

--- Going Dutch/ViewControllers/MCSharedBillTableViewController.m
+++ Going Dutch/ViewControllers/MCSharedBillTableViewController.m
@@ -140,10 +140,15 @@
     }
 }
 
-- (void)openFirstPaymentWithoutAPayer
-{
+- (void)openFirstPaymentWithoutAPayer {
     // This opens the payment detail view with the first payment on the tonightsBill which, doesn't have a payer.
-    [self performSegueWithIdentifier:@"openFirstPaymentWithoutPayer" sender:self];
+    UIUserInterfaceSizeClass horizontalSizeClass = self.traitCollection.horizontalSizeClass;
+    UIUserInterfaceSizeClass verticalSizeClass = self.traitCollection.verticalSizeClass;
+    if (horizontalSizeClass == UIUserInterfaceSizeClassRegular && verticalSizeClass == UIUserInterfaceSizeClassRegular) {
+        [self performSegueWithIdentifier:@"openFirstPaymentWithoutPayer_iPad" sender:self];
+    } else {
+        [self performSegueWithIdentifier:@"openFirstPaymentWithoutPayer" sender:self];
+    }
 }
 
 #pragma mark - NSNotification
@@ -158,9 +163,14 @@
 
 #pragma mark - ShowPayment
 
-- (void)show:(MCPayment *)payment
-{
-    [self performSegueWithIdentifier:@"openPaymentWithMissingData" sender:self];
+- (void)show:(MCPayment *)payment {
+    UIUserInterfaceSizeClass horizontalSizeClass = self.traitCollection.horizontalSizeClass;
+    UIUserInterfaceSizeClass verticalSizeClass = self.traitCollection.verticalSizeClass;
+    if (horizontalSizeClass == UIUserInterfaceSizeClassRegular && verticalSizeClass == UIUserInterfaceSizeClassRegular) {
+        [self performSegueWithIdentifier:@"openPaymentWithMissingData_iPad" sender:self];
+    } else {
+        [self performSegueWithIdentifier:@"openPaymentWithMissingData" sender:self];
+    }
 }
 
 #pragma mark - MFMailViewControllerDelegate
@@ -374,6 +384,26 @@
         UINavigationController *navController = (UINavigationController *)segue.destinationViewController;
         SolutionViewController *destination = navController.viewControllers.firstObject;
         [destination updateEvent:_tonightsBill andSendMailDelegate:_mailDelegate andAdEngine:self.adEngine];
+    } else if ([segue.identifier isEqualToString:@"openFirstPaymentWithoutPayer_iPad"]) {
+        NSParameterAssert([[[segue destinationViewController] viewControllers][0] conformsToProtocol:@protocol(MCThisPaymentProtocol)]);
+        UINavigationController *navigationController = (UINavigationController *)segue.destinationViewController;
+        PaymentViewController *destination = (PaymentViewController *)navigationController.viewControllers[0];
+        destination.thisPayment = [_tonightsBill getFirstPaymentWithoutAPayer];
+        destination.tonightsBill = _tonightsBill;
+        if (@available(iOS 13.0, *)) {
+            UINavigationController *navController = (UINavigationController *)segue.destinationViewController;
+            navController.modalInPresentation = YES;
+        }
+    } else if ([segue.identifier isEqualToString:@"openPaymentWithMissingData_iPad"]) {
+        UINavigationController *navigationController = (UINavigationController *)segue.destinationViewController;
+        PaymentViewController *destination = navigationController.viewControllers[0];
+        destination.tonightsBill = _tonightsBill;
+        destination.thisPayment = _forOpenPaymentWithMissingDataForSegue;
+        _forOpenPaymentWithMissingDataForSegue = nil;
+        if (@available(iOS 13.0, *)) {
+            UINavigationController *navController = (UINavigationController *)segue.destinationViewController;
+            navController.modalInPresentation = YES;
+        }
     } else if ([segue.identifier isEqualToString:@"newPayment_iPad"]) {
         UINavigationController *navController = (UINavigationController *)segue.destinationViewController;
         if (@available(iOS 13.0, *)) {
