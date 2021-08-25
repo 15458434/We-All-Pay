0491f3d1c717a8093f131791a7165400aa78946f
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-08-25 10:35:27 +0200

    #869 - Added stringDict for the one sentence in the MailComposer.

---
+++ Going Dutch/Miscellaneous/Mail/MailComposeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MailComposeViewController.swift
+//  We all pay
+//
+//  Created by Mark Cornelisse on 07/09/2016.
+//  Copyright © 2016 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+@objc(MCMailComposeViewController) final class MailComposeViewController: MFMailComposeViewController {
+
+    // MARK: UINavigationController
+    
+    // MARK: UIViewController
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: UIResponder
+    
+    // MARK: NSObject
+}
