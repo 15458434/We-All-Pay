e77064154a9ebf3e6a9e4bddd36c5302fb257b7f
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-10-13 11:45:15 +0200

    #948 - Removed the CoreDataGeneratedAccessors from the MCPerson class.

--- Going Dutch/MCPerson.h
+++ Going Dutch/MCPerson.h
@@ -16,27 +16,3 @@ __attribute__((objc_subclassing_restricted))
 @interface MCPerson : NSManagedObject
 
 @end
-
-@interface MCPerson (CoreDataGeneratedAccessors)
-
-- (void)addEmailAddressObject:(MCEmailAddress *)value;
-- (void)removeEmailAddressObject:(MCEmailAddress *)value;
-- (void)addEmailAddress:(NSSet *)values;
-- (void)removeEmailAddress:(NSSet *)values;
-
-- (void)addPaymentsObject:(MCPayment *)value;
-- (void)removePaymentsObject:(MCPayment *)value;
-- (void)addPayments:(NSSet *)values;
-- (void)removePayments:(NSSet *)values;
-
-- (void)addSharedBillObject:(MCSharedBill *)value;
-- (void)removeSharedBillObject:(MCSharedBill *)value;
-- (void)addSharedBill:(NSSet *)values;
-- (void)removeSharedBill:(NSSet *)values;
-
-- (void)addSharingPaymentObject:(MCPaymentPresence *)value;
-- (void)removeSharingPaymentObject:(MCPaymentPresence *)value;
-- (void)addSharingPayment:(NSSet *)values;
-- (void)removeSharingPayment:(NSSet *)values;
-
-@end
