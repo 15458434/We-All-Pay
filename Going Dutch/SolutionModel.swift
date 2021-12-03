02822fb69570750612a3af9bcc82465dd9439545
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-12-03 12:50:56 +0100

    #957 - MCReturnPaymentTableViewController has been updated to support the new translation system.

---
+++ Going Dutch/SolutionModel.swift
@@ -0,0 +1,28 @@
+//
+//  SolutionModel.swift
+//  We all pay
+//
+//  Created by Mark Cornelisse on 03/12/2021.
+//  Copyright © 2021 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+
+@objc(MCSolutionModel) final class SolutionModel: NSObject {
+    @objc(MCSolutionViewSectionTitle) enum SectionTitle: UInt {
+        case whoOwesWho = 0
+        case totalOwes = 1
+        case totalPaid = 2
+    }
+    
+    @objc(sectionTitleForSection:) func sectionTitle(for section: SectionTitle) -> String {
+        switch section {
+        case .whoOwesWho:
+            return NSLocalizedString("solution_view_section_title_who_owes_who", value: "Who owes whom", comment: "Section title in the solution screen that shows the title of the section that shows who owes who what how much money")
+        case .totalOwes:
+            return NSLocalizedString("solution_view_section_title_total_owes", value: "Total owes", comment: "Section title in the solution screen that shows the title of the section that shows who owes how much to the group")
+        case .totalPaid:
+            return NSLocalizedString("solution_view_section_title_total_paid", value: "Total paid", comment: "Section title in the solution screen that shows the title of the section that shows who paid how much on the entire event")
+        }
+    }
+}
