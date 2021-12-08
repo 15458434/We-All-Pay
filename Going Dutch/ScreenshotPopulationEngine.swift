16035e1b5b4500bdbc5696dbd6079518e37cdb90
Mark Cornelisse <m.p.cornelisse@gmail.com>
2021-12-08 11:22:00 +0100

    #957 - App Store Screen shot translations has been properly added.

--- Going Dutch/ScreenshotPopulationEngine.swift
+++ Going Dutch/ScreenshotPopulationEngine.swift
@@ -20,43 +20,43 @@ import CoreData
     
     @objc public func populate() {
         var david: (firstName: String, lastName: String, emailAddress: String) = {
-            let firstName = NSLocalizedString("David", comment: "first name example for app store screenshots")
-            let lastName = NSLocalizedString("Tucker", comment: "last name example for appstore screenshots")
-            let emailAddress = NSLocalizedString("davidtucker@somewhere.com", comment: "example for appstore screenshots")
+            let firstName = NSLocalizedString("app_store_screenshots_person_firstname_1", value: "David", comment: "first name example for app store screenshots")
+            let lastName = NSLocalizedString("app_store_screenshots_person_lastname_1", value: "Tucker", comment: "last name example for appstore screenshots")
+            let emailAddress = NSLocalizedString("app_store_screenshots_person_email_1", value: "davidtucker@somewhere.com", comment: "example for appstore screenshots")
             return (firstName: firstName, lastName: lastName, emailAddress: emailAddress)
         }()
         
         var jake: (firstName: String, lastName: String, emailAddress: String) = {
-            let firstName = NSLocalizedString("Jake", comment: "first name example for app store screenshots")
-            let lastName = NSLocalizedString("Nichelson", comment: "last name example for appstore screenshots")
-            let emailAddress = NSLocalizedString("jake@earth.com", comment: "example for appstore screenshots")
+            let firstName = NSLocalizedString("app_store_screenshots_person_firstname_2", value: "Jake", comment: "first name example for app store screenshots")
+            let lastName = NSLocalizedString("app_store_screenshots_person_lastname_2", value: "Nichelson", comment: "last name example for appstore screenshots")
+            let emailAddress = NSLocalizedString("app_store_screenshots_person_email_2", value: "jake@earth.com", comment: "example for appstore screenshots")
             return (firstName: firstName, lastName: lastName, emailAddress: emailAddress)
         }()
 
         var ann: (firstName: String, lastName: String, emailAddress: String) = {
-            let firstName = NSLocalizedString("Ann", comment: "first name example for app store screenshots")
-            let lastName = NSLocalizedString("Burwell", comment: "last name example for appstore screenshots")
-            let emailAddress = NSLocalizedString("ann_burwell@work.com", comment: "example for appstore screenshots")
+            let firstName = NSLocalizedString("app_store_screenshots_person_firstname_3", value: "Ann", comment: "first name example for app store screenshots")
+            let lastName = NSLocalizedString("app_store_screenshots_person_lastName_3", value: "Burwell", comment: "last name example for appstore screenshots")
+            let emailAddress = NSLocalizedString("app_store_screenshots_person_email_3", value: "ann_burwell@work.com", comment: "example for appstore screenshots")
             return (firstName: firstName, lastName: lastName, emailAddress: emailAddress)
         }()
 
         var lauren: (firstName: String, lastName: String, emailAddress: String) = {
-            let firstName = NSLocalizedString("Lauren", comment: "first name example for app store screenshots")
-            let lastName = NSLocalizedString("Mortimer", comment: "last name example for appstore screenshots")
-            let emailAddress = NSLocalizedString("l.d.mortimer@internet.com", comment: "example for appstore screenshots")
+            let firstName = NSLocalizedString("app_store_screenshots_person_firstname_4", value: "Lauren", comment: "first name example for app store screenshots")
+            let lastName = NSLocalizedString("app_store_screenshots_person_lastname_4" , value: "Mortimer", comment: "last name example for appstore screenshots")
+            let emailAddress = NSLocalizedString("app_store_screenshots_person_email_4", value: "l.d.mortimer@internet.com", comment: "example for appstore screenshots")
             return (firstName: firstName, lastName: lastName, emailAddress: emailAddress)
         }()
 
         var sheila: (firstName: String, lastName: String, emailAddress: String) = {
-            let firstName = NSLocalizedString("Sheila", comment: "first name example for app store screenshots")
-            let lastName = NSLocalizedString("Miller", comment: "last name example for appstore screenshots")
-            let emailAddress = NSLocalizedString("mailsheila@home.com", comment: "example for appstore screenshots")
+            let firstName = NSLocalizedString("app_store_screenshots_person_firstname_5", value: "Sheila", comment: "first name example for app store screenshots")
+            let lastName = NSLocalizedString("app_store_screenshots_person_lastname_5", value: "Miller", comment: "last name example for appstore screenshots")
+            let emailAddress = NSLocalizedString("app_store_screenshots_person_email_5", value: "mailsheila@home.com", comment: "example for appstore screenshots")
             return (firstName: firstName, lastName: lastName, emailAddress: emailAddress)
         }()
         
         func createConcert() {
             let concert = MCSharedBill.add(to: managedObjectContext)!
-            concert.tripName = NSLocalizedString("Concert", comment: "event name example for app store screenshots")
+            concert.tripName = NSLocalizedString("app_store_screenshots_eventname_1", value: "Concert", comment: "event name example for app store screenshots")
             let david = concert.add(demoPerson: david)
             concert.add(demoPerson: sheila)
             concert.add(demoPerson: lauren)
@@ -71,7 +71,7 @@ import CoreData
         }
         func createTheatre() {
             let theatre = MCSharedBill.add(to: managedObjectContext)!
-            theatre.tripName = NSLocalizedString("Theatre", comment: "event name example for app store screenshots")
+            theatre.tripName = NSLocalizedString("app_store_screenshots_eventname_2", value: "Theatre", comment: "event name example for app store screenshots")
             theatre.add(demoPerson: david)
             theatre.add(demoPerson: jake)
             theatre.add(demoPerson: ann)
@@ -88,7 +88,7 @@ import CoreData
         }
         func createMonthlyFoodCosts() {
             let foodCosts = MCSharedBill.add(to: managedObjectContext)!
-            foodCosts.tripName = NSLocalizedString("This month food costs", comment: "event name example for app store screenshots")
+            foodCosts.tripName = NSLocalizedString("app_store_screenshots_eventname_3", value: "This month food costs", comment: "event name example for app store screenshots")
             let sheila = foodCosts.add(demoPerson: sheila)
             foodCosts.add(demoPerson: david)
             
@@ -101,7 +101,7 @@ import CoreData
         }
         func createCampingTrip() {
             let campingTrip = MCSharedBill.add(to: managedObjectContext)!
-            campingTrip.tripName = NSLocalizedString("Camping trip", comment: "event name example for app store screenshots")
+            campingTrip.tripName = NSLocalizedString("app_store_screenshots_eventname_4", value: "Camping trip", comment: "event name example for app store screenshots")
             campingTrip.add(demoPerson: david)
             campingTrip.add(demoPerson: jake)
             let ann = campingTrip.add(demoPerson: ann)
@@ -117,20 +117,20 @@ import CoreData
         }
         func createDinner() {
             let dinner = MCSharedBill.add(to: managedObjectContext)!
-            dinner.tripName = NSLocalizedString("Dinner", comment: "event name example for app store screenshots")
+            dinner.tripName = NSLocalizedString("app_store_screenshots_eventname_5", value: "Dinner", comment: "event name example for app store screenshots")
             dinner.add(demoPerson: sheila)
             let david = dinner.add(demoPerson: david)
             
             let sushi = dinner.addPayment()!
             sushi.payingPerson = david
-            sushi.descriptionOfPayment = NSLocalizedString("Sushi", comment: "payed item example for app store screenshots")
+            sushi.descriptionOfPayment = NSLocalizedString("app_store_screenshots_eventname_5_payment_1", value: "Sushi", comment: "payed item example for app store screenshots")
             sushi.money = NSNumber(value: 62.00)
             sushi.categoryId = CategoryPictureStoreController.PaymentCategory.dinner.number
             sushi.recalculateAveragePeopleOweAndStore()
         }
         func createMovie() {
             let movie = MCSharedBill.add(to: managedObjectContext)!
-            movie.tripName = NSLocalizedString("Movie", comment: "event name example for app store screenshots")
+            movie.tripName = NSLocalizedString("app_store_screenshots_eventname_6", value: "Movie", comment: "event name example for app store screenshots")
             movie.add(demoPerson: sheila)
             let ann = movie.add(demoPerson: ann)
             let jake = movie.add(demoPerson: jake)
@@ -139,19 +139,19 @@ import CoreData
             
             let tickets = movie.addPayment()!
             tickets.payingPerson = jake
-            tickets.descriptionOfPayment = NSLocalizedString("Tickets", comment: "payed item example for app store screenshots")
+            tickets.descriptionOfPayment = NSLocalizedString("app_store_screenshots_eventname_6_payment_1", value: "Tickets", comment: "payed item example for app store screenshots")
             tickets.money = NSNumber(value: 60.00)
             tickets.categoryId = CategoryPictureStoreController.PaymentCategory.tickets.number
             
             let popcorn = movie.addPayment()!
             popcorn.payingPerson = ann
-            popcorn.descriptionOfPayment = NSLocalizedString("Popcorn", comment: "payed item example for app store screenshots")
+            popcorn.descriptionOfPayment = NSLocalizedString("app_store_screenshots_eventname_6_payment_2", value: "Popcorn", comment: "payed item example for app store screenshots")
             popcorn.money = NSNumber(value: 15.00)
             popcorn.categoryId = CategoryPictureStoreController.PaymentCategory.dinner.number
             
             let softDrinks = movie.addPayment()!
             softDrinks.payingPerson = lauren
-            softDrinks.descriptionOfPayment = NSLocalizedString("Soft drinks", comment: "payed item example for app store screenshots")
+            softDrinks.descriptionOfPayment = NSLocalizedString("app_store_screenshots_eventname_6_payment_3", value: "Soft drinks", comment: "payed item example for app store screenshots")
             softDrinks.money = NSNumber(value: 24.78)
             softDrinks.categoryId = CategoryPictureStoreController.PaymentCategory.drinks.number
             
