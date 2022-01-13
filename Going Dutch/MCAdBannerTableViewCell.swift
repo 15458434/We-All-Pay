b20a8cef971a1749ec9510692bd1089f509dd0c2
Mark Cornelisse <m.p.cornelisse@gmail.com>
2022-01-13 14:47:58 +0100

    #983 - SolutionView now presents the space for the add before the ad shows.

--- Going Dutch/MCAdBannerTableViewCell.swift
+++ Going Dutch/MCAdBannerTableViewCell.swift
@@ -11,12 +11,18 @@ import GoogleMobileAds;
 
 @objc(MCAdBannerTableViewCell) final class AdBannerTableViewCell: UITableViewCell {
     private weak var bannerView: GADBannerView?
+    @objc private var itemModel: AdSectionItemsModel?
+    private var adStatusObserver: NSKeyValueObservation?
     
-    @objc(updateBannerView:) func update(bannerView: GADBannerView) {
+    @objc(updateBannerView:andItemsModel:) func update(bannerView: GADBannerView, itemModel: AdSectionItemsModel) {
         if let existingBannerView = self.bannerView {
             let constraints = existingBannerView.constraints
             existingBannerView.removeConstraints(constraints)
             existingBannerView.removeFromSuperview()
+        }
+        if self.itemModel != nil {
+            self.adStatusObserver = nil
+            self.itemModel = nil
         }
         bannerView.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(bannerView)
@@ -27,9 +33,31 @@ import GoogleMobileAds;
         let height: CGFloat = CGFloat(RemoteConfigEngine().number(for: .solutionAdBannerHeight)!.doubleValue)
         self.bannerView!.heightAnchor.constraint(equalToConstant: height).isActive = true
         self.bannerView!.widthAnchor.constraint(equalToConstant: 320).isActive = true
+        
+        self.itemModel = itemModel
+        adStatusObserver = self.observe(\.itemModel!.adStatus, options: [.initial, .new], changeHandler: { mySelf, change in
+            switch mySelf.itemModel!.adStatus {
+            case .isNotShowing:
+                UIView.animate(withDuration: 0.27) {
+                    self.bannerView!.alpha = 0.0
+                }
+            case .isShowing:
+                UIView.animate(withDuration: 0.27) {
+                    self.bannerView!.alpha = 1.0
+                }
+            }
+        })
     }
     
     // MARK: UITableViewCell
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        adStatusObserver = nil
+        itemModel = nil
+        bannerView = nil
+    }
     
     // MARK: UIView
     
