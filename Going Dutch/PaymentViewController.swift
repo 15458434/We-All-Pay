55929da3bb84fdb7c58d761f162dbbca1924c3c0
Mark Cornelisse <m.p.cornelisse@gmail.com>
2024-07-09 10:07:33 +0200

    #1018 - Cleanup unused stuff.

--- Going Dutch/PaymentViewController.swift
+++ Going Dutch/PaymentViewController.swift
@@ -26,7 +26,7 @@ enum CancelButtonPressed {
     case notPressed, isPressed
 }
 
-final class PaymentViewController: MCGenericAdBannerTableViewController, AdBannerEngineDelegate, MCDismissKeyboardProtocol, MCPathComponentsToOpenProtocol, UITextFieldDelegate, NSFetchedResultsControllerDelegate, PaymentStateModelProtocol {
+final class PaymentViewController: MCGenericAdBannerTableViewController, AdBannerEngineDelegate, MCDismissKeyboardProtocol, MCPathComponentsToOpenProtocol, UITextFieldDelegate, NSFetchedResultsControllerDelegate {
 
     // MARK: IB Outlet
     @IBOutlet weak var headerView: UITableViewHeaderFooterView!
@@ -194,12 +194,6 @@ final class PaymentViewController: MCGenericAdBannerTableViewController, AdBanne
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         debugPrint("controllerDidChangeContent")
         tableView.endUpdates()
-    }
-    
-    // MARK: PaymentStateModelProtocol
-    
-    @objc var paymentStateModel: PaymentModel {
-        return self.model
     }
     
     // MARK: AdBannerEngineDelegate
